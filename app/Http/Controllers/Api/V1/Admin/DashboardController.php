2a8b9cadbbc7b50dfb38a57cdb372972e12ddbf5
matcha <nengindah41@gmail.com>
2023-04-16 00:49:26 +0700

    [HK] dahsboard admin

--- app/Http/Controllers/Api/V1/Admin/DashboardController.php
+++ app/Http/Controllers/Api/V1/Admin/DashboardController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    public function dahsboard()
+    public function dashboard()
     {
+        $totalUser = DB::table('users')->count();
+        $totalTransaction = DB::table('transactions')->count();
+        $totalCourse = DB::table('courses')->count();
+        $rate = DB::table('rates')->latest()->first();
+
+
         return response()->json([
-            'message' => 'Dashboard',
-        ], 200);
+            'user' => $totalUser,
+            'transaction' => $totalTransaction,
+            'course' => $totalCourse,
+            'rate' => $rate
+        ]);
     }
 }
