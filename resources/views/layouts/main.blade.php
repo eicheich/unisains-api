7d7617ad330fd05a2904988c0aa3112f3f7822d5
matcha <nengindah41@gmail.com>
2023-05-06 11:14:22 +0700

    [HK] UI Dashboard

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -151,10 +151,8 @@
     </div>
     @include('layouts.navbar')
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         @yield('content')
 
-      </div>
     </main>
     <script src="{{asset('js/bootstrap.js')}}"></script>
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
