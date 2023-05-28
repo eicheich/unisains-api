e6c663cace751433bff4ac04cd01961f2fff49d1
eicheich <nengindah41@gmail.com>
2023-05-28 16:53:47 +0700

    [HK] update and fix route login

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::get('/acces-denied', function () {
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
-        Route::get('login', [AuthController::class, 'login']);
+        Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
         Route::middleware('auth:sanctum')->group(function () {
             Route::post('logout', [AuthController::class, 'logout']);
