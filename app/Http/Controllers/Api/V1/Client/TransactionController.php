c88cf7bc70525186fbc59f184ab89155baad0b8b
Husnul Khotimah <nwngindah41@gmail.com>
2023-06-03 09:56:01 +0700

    [HK] try schedule due time

--- app/Http/Controllers/Api/V1/Client/TransactionController.php
+++ app/Http/Controllers/Api/V1/Client/TransactionController.php
@@ -39,6 +39,7 @@ class TransactionController extends Controller
                     'total_price' => null
                 ]);
                 DB::commit();
+                $this->schedule($transaction);
                 return response()->json([
                     'message' => 'Transaction added',
                 ], 201);
@@ -50,5 +51,47 @@ class TransactionController extends Controller
             }
             # code...
         }
+
     }
+    public function all()
+    {
+        $user = Auth::user();
+        // check if transaction is exist
+        $transaction = DB::table('transactions')->where('user_id', $user->id)->get();
+        if ($transaction->count() == 0) {
+            return response()->json([
+                'message' => 'Transaction not found',
+            ], 404);
+        }
+        $transactions = DB::table('transactions')
+            ->join('courses', 'transactions.course_id', '=', 'courses.id')
+            ->select('transactions.*', 'courses.title_course', 'courses.image_course', 'courses.price', 'courses.is_paid', 'courses.description')
+            ->where('transactions.user_id', $user->id)
+            ->get();
+        return response()->json([
+            'message' => 'Success',
+            'data' => $transactions,
+        ], 200);
+            
+    }
+
+    public function schedule(schedule $schedule, $transaction)
+    {
+        $schedule->call(function () use ($transaction) {
+            $transaction = DB::table('transactions')->where('id', $transaction->id)->first();
+            if ($transaction->status == 'pending') {
+                if ($transaction->created_at < now()->subMinutes(1)) {
+                    DB::table('transactions')->where('id', $transaction->id)->update([
+                        'status' => 'failed',
+                    ]);
+                }
+                
+            }
+        })->everyMinute();
+    }
+    // {
+    //     $user = Auth::user();
+    //     // delete transaction in 24 hours
+    //     $transaction = DB::table('transactions')->where('user_id', $user->id)->where('created_at', '<', now()->subHours(24))->delete();
+    // }
 }
