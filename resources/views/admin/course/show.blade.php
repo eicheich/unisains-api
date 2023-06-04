0686b57c68165101c1daa988aa2a39eac92e7847
eicheich <nengindah41@gmail.com>
2023-06-04 20:32:27 +0700

    [HK] alert session

--- resources/views/admin/course/show.blade.php
+++ resources/views/admin/course/show.blade.php
@@ -2,11 +2,7 @@
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Detail Kursus</h1>
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+        @include('layouts.session')
     </div>
     <div class="container mt-5">
         <div class="row">
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.main')
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+        @include('layouts.session')
         <h1 class="h2">Dashboard</h1>
     </div>
     <div class="row">
