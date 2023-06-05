b7abaa358b46a3d624cf8cb9fbb4cd1da9e14953
eicheich <nengindah41@gmail.com>
2023-06-05 22:19:06 +0700

    [HK]

--- app/Http/Controllers/Api/V1/Client/CourseController.php
+++ app/Http/Controllers/Api/V1/Client/CourseController.php
@@ -63,48 +63,48 @@ class CourseController extends Controller
 
 
     public function show($id)
-{
-    $course = DB::table('courses')
-        ->where('courses.id', $id)
-        ->join('categories', 'courses.category_id', '=', 'categories.id')
-        ->select('courses.*', 'categories.name_category')
-        ->first();
-    $module = DB::table('modules')->where('course_id', $id)->get();
-    $module_rangkuman = DB::table('module_rangkuman')->where('course_id', $id)->get();
-    $ar = DB::table('augmented_realities')->where('course_id', $id)->first();
-    $quiz = DB::table('quizzes')->where('course_id', $id)->get();
+    {
+        $course = DB::table('courses')
+            ->where('courses.id', $id)
+            ->join('categories', 'courses.category_id', '=', 'categories.id')
+            ->select('courses.*', 'categories.name_category')
+            ->first();
+        $module = DB::table('modules')->where('course_id', $id)->get();
+        $module_rangkuman = DB::table('module_rangkuman')->where('course_id', $id)->get();
+        $ar = DB::table('augmented_realities')->where('course_id', $id)->first();
+        $quiz = DB::table('quizzes')->where('course_id', $id)->get();
 
-    if ($course) {
-        // Mengubah URL gambar dan video menjadi URL yang valid
-        $course->image_course = UrlHelper::formatImageCourseUrl($course->image_course);
-        $course->certificate_course = UrlHelper::formatCertiCourseUrl ($course->certificate_course);
+        if ($course) {
+            // Mengubah URL gambar dan video menjadi URL yang valid
+            $course->image_course = UrlHelper::formatImageCourseUrl($course->image_course);
+            $course->certificate_course = UrlHelper::formatCertiCourseUrl($course->certificate_course);
 
-        foreach ($module as $moduleItem) {
-            $moduleItem->image_module = UrlHelper::formatImageModuleUrl ($moduleItem->image_module);
+            foreach ($module as $moduleItem) {
+                $moduleItem->image_module = UrlHelper::formatImageModuleUrl($moduleItem->image_module);
+            }
+
+            foreach ($module_rangkuman as $rangkumanItem) {
+                $rangkumanItem->video_rangkuman = UrlHelper::formatVideoUrl($rangkumanItem->video_rangkuman);
+            }
+
+
+            if ($ar) {
+                $ar->image_ar = asset('storage/images/ar/' . $ar->image_ar);
+            }
+
+            return response()->json([
+                'course' => $course,
+                'module' => $module,
+                'module_rangkuman' => $module_rangkuman,
+                'ar' => $ar,
+                'quiz' => $quiz,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Course not found'
+            ], 404);
         }
-
-        foreach ($module_rangkuman as $rangkumanItem) {
-            $rangkumanItem->video_rangkuman = UrlHelper::formatVideoUrl ( $rangkumanItem->video_rangkuman);
-        }
-
-
-        if ($ar) {
-            $ar->image_ar = asset('storage/images/ar/' . $ar->image_ar);
-        }
-
-        return response()->json([
-            'course' => $course,
-            'module' => $module,
-            'module_rangkuman' => $module_rangkuman,
-            'ar' => $ar,
-            'quiz' => $quiz,
-        ], 200);
-    } else {
-        return response()->json([
-            'message' => 'Course not found'
-        ], 404);
     }
-}
 
 
     public function preview($id)
