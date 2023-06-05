b9ca23e3581d0bb96067cdfa011d86618bf7b674
eicheich <nengindah41@gmail.com>
2023-06-05 21:02:23 +0700

    [HK] image and video on show course

--- app/Http/Controllers/Api/V1/Client/CourseController.php
+++ app/Http/Controllers/Api/V1/Client/CourseController.php
@@ -62,28 +62,46 @@ class CourseController extends Controller
 
 
     public function show($id)
-    {
-        $course = DB::table('courses')
-            ->where('courses.id', $id)
-            ->join('categories', 'courses.category_id', '=', 'categories.id')
-            ->select('courses.*', 'categories.name_category')
-            ->first();
-        $module = DB::table('modules')->where('course_id', $id)->get();
-        $module_rangkuman = DB::table('module_rangkuman')->where('course_id', $id)->get();
-        $ar = DB::table('augmented_realities')->where('course_id', $id)->first();
+{
+    $course = DB::table('courses')
+        ->where('courses.id', $id)
+        ->join('categories', 'courses.category_id', '=', 'categories.id')
+        ->select('courses.*', 'categories.name_category')
+        ->first();
+    $module = DB::table('modules')->where('course_id', $id)->get();
+    $module_rangkuman = DB::table('module_rangkuman')->where('course_id', $id)->get();
+    $ar = DB::table('augmented_realities')->where('course_id', $id)->first();
 
-        if ($course) {
-            return response()->json([
-                'course' => $course,
-                'module' => $module,
-                'module_rangkuman' => $module_rangkuman,
-                'ar' => $ar,
-            ], 200);
-        } else
-            return response()->json([
-                'message' => 'course not found'
-            ], 404);
+    if ($course) {
+        // Mengubah URL gambar dan video menjadi URL yang valid
+        $course->image_course = asset('storage/images/thumbnail_course/' . $course->image_course);
+        $course->certificate_course = asset('storage/images/certificate/' . $course->certificate_course);
+
+        foreach ($module as $moduleItem) {
+            $moduleItem->image_module = asset('storage/images/module/' . $moduleItem->image_module);
+        }
+
+        foreach ($module_rangkuman as $rangkumanItem) {
+            $rangkumanItem->video_rangkuman = asset('storage/video/rangkuman/' . $rangkumanItem->video_rangkuman);
+        }
+
+        if ($ar) {
+            $ar->image_ar = asset('storage/images/ar/' . $ar->image_ar);
+        }
+
+        return response()->json([
+            'course' => $course,
+            'module' => $module,
+            'module_rangkuman' => $module_rangkuman,
+            'ar' => $ar,
+        ], 200);
+    } else {
+        return response()->json([
+            'message' => 'Course not found'
+        ], 404);
     }
+}
+
 
     public function preview($id)
     {
