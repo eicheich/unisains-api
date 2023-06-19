e649ede2774ed66814e4a538f08fb7809f5b244b
eicheich <nengindah41@gmail.com>
2023-06-19 12:56:02 +0700

    [HK] change veritrans to midtrans and fix some bug

--- app/Http/Controllers/Api/V1/Client/PaymentController.php
+++ app/Http/Controllers/Api/V1/Client/PaymentController.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Veritrans_Config;
-use Veritrans_Snap;
-use Veritrans_Transaction;
+use Midtrans\Config;
+use Midtrans\Snap;
+use Midtrans\Transaction;
 
 class PaymentController extends Controller
 {
@@ -24,36 +24,39 @@ class PaymentController extends Controller
                 'errors' => $validator->errors(),
             ], 422);
         }
-        Veritrans_Config::$serverKey = env('MIDTRANS_SERVER_KEY');
-        Veritrans_Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
-        Veritrans_Config::$isSanitized = true;
-        Veritrans_Config::$is3ds = true;
+
+        Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        Config::$isSanitized = true;
+        Config::$is3ds = true;
+
         $total = DB::table('transactions')->where('id', $request->transaction_id)->first();
+
         if (!$total || !isset($total->total_price)) {
             return response()->json([
                 'status' => 'error',
                 'message' => 'Invalid transaction data',
             ], 400);
         }
+
         $params = array(
             'transaction_details' => array(
                 'order_id' => rand(),
                 'gross_amount' => (int)$total->total_price,
-            )
+            ),
         );
+
         try {
-            $snapToken = Veritrans_Snap::getSnapToken($params);
+            $snapToken = Snap::getSnapToken($params);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => $e->getMessage()
+                'message' => $e->getMessage(),
             ], 500);
         }
+
         return response()->json([
             'snap_token' => $snapToken,
         ]);
     }
-
-
-
 }
