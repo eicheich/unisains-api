a8d70af0a6f71b1a347e2d39be3747939f5264ae
eicheich <nengindah41@gmail.com>
2023-08-14 12:08:07 +0700

    store

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Wishlist extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
